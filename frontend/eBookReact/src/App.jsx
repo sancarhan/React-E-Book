b27105af527a6da2c9c9f6ae3554c9376f07e777
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-20 16:03:54 +0300

    Update App.jsx
    .

--- frontend/eBookReact/src/App.jsx
+++ frontend/eBookReact/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 const App = () => {
   return (
-    <div >
-      App
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
