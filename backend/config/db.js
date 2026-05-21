b56cc3e3d4d4ef796b1c87dce8ba8b591c31367f
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-21 17:42:30 +0300

    .

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,14 @@
-const
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+ try {
+  await mongoose.connect(process.env.MONGO_URI, {});
+  console.log("MongoDB Bağlandı");
+  
+ } catch (error) {
+  console.error("MongoDB bağlantısında hata", err);
+  process.exit(1);
+ }
+};
+
+module.exports = connectDB;
