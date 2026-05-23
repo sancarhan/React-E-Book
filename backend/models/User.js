16046b4d4f82a437c854de6c47b386d2967039bf
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-23 11:43:46 +0300

    .

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,3 +31,18 @@ const userSchema = new mongoose.Schema(
  } ,
  { timestamps: true}
 );
+
+userSchema.pre("save", async function (next) {
+ if ( !this.isModified("password")) return next();
+ const salt = await bcrypt.genSalt(10);
+ this.password = await bcrypt.hash(this.password, salt);
+ next();
+});
+
+userSchema.methods.matchPassword = async function (enterPassword) {
+ return await bcrypt.compare(enterPassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
+
+
