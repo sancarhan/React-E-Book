38d62020096e166d4ad24576f6a49cec2d12a360
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-23 12:22:14 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require('./routes/authRoutes');
 
-const  app = express();
+const app = express();
 
 app.use(
  cors({
   origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
  })
 );
@@ -20,11 +20,11 @@ connectDB();
 
 app.use(express.json());
 
-// Statik Dosyalar
 app.use("/backend/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/auth", authRoutes);
 
-// Server başltma
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Port ${PORT} de server çalışıyor `));
+app.listen(PORT, () => console.log(`Server port ${PORT}'de çalışıyor`));
+
+
