8b0eb2f2ae22fb0ee4436448a819492e5055a3bd
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-05-24 15:32:11 +0300

    .

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,13 +3,37 @@ const User = require("../models/User");
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 };
+// -------------------------------------------------------------------------
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Lütfen tüm alanları doldurun"});
+    }
+
+    // Kullanıcı kontrol
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({message: "Kullanıcı zaten mevcut"})
+    }
+
+    // KullanıcıOluşturma
+    const user = await User.create({name, email, password});
+
+    if (user) {
+      res.status(201).json({
+        message: "Kullanıcı başarıyla kaydoldu",
+        token: generateToken(user._id),
+      });
+    }else{
+      res.status(400).json({ message: "Geçersiz kullanıcı"});
+    }
+
   } catch (error) {
     res.status(500).json({ message: "Server Hatası" });
   }
 };
+// ---------------------------------------------------------------------
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -17,15 +41,18 @@ exports.loginUser = async (req, res) => {
     res.status(500).json({ message: "Server Hatası" });
   }
 };
-exports.getProfil = async (req, res) => {
+// --------------------------------------------------------------------
+exports.getProfile = async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json({ message: "Server Hatası" });
   }
 };
+// ---------------------------------------------------------------------
 exports.updateUserProfile = async (req, res) => {
   try {
   } catch (error) {
     res.status(500).json({ message: "Server Hatası" });
   }
 };
+
